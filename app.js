0b711b5fd8f37646c3be3a0cc869c73716532e11
akat32 <baduk32127@gmail.com>
2017-11-14 20:56:16 +0900

    fix cors2

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var rndstring = require('randomstring');
 var CORS = require('cors')();
+app.use(CORS);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '1gb', extended: false }));
 require('./mongo');
 require('./func');
 var auth = require('./routes/auth')(express.Router(),Users,rndstring);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({limit: '1gb', extended: false }));
-app.use(CORS);
 app.use('/auth', auth);
 app.listen(3000, (req,res)=>{
   console.log('Server Port on 3000');
