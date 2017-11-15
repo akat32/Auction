64fefb124535513c5b731621d0fcf3ff069cb2eb
akat32 <baduk32127@gmail.com>
2017-11-15 23:09:12 +0900

    fix spalling

--- routes/finditem.js
+++ routes/finditem.js
@@ -1,7 +1,7 @@
 module.exports = (router, List)=>{
   router.post('/name', async (req,res)=>{
-    const rqurey = new RegExp(req.body.item_name); 
-    var result = await List.find({item_name : rqurey});
+    const rquery = new RegExp(req.body.item_name); 
+    var result = await List.find({item_name : rquery});
     if(!result) return res.status(404).send("Not Found Item!");
     else await res.status(200).json(result);
   })
