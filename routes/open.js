b8a25fe2e8e8499a776a9124c194fde2e485d41f
akat32 <baduk32127@gmail.com>
2017-11-17 00:04:45 +0900

    fix open

--- routes/open.js
+++ routes/open.js
@@ -1,6 +1,6 @@
 module.exports = (router, List, Users)=>{
   router.post('/item', async(req,res)=>{
-    var item = await List.findOne(req.body.item_token);
+    var item = await List.findOne(req.body);
     if(!item) return res.status(404).send('Item Not found!');
     await res.status(200).json(item);
   })
