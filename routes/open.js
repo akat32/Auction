286fdec45020c87276f41861b8fe2aa22801801e
akat32 <baduk32127@gmail.com>
2017-11-16 16:54:58 +0900

    add open.js

--- routes/open.js
+++ routes/open.js
@@ -2,6 +2,8 @@ module.exports = (router, List, Users)=>{
   router.post('/item', async(req,res)=>{
     var find_token = new List(req.body.item_token);
     var result = await find_token.findOne();
+    if(!result) return res.status(404).send('Item Not found!');
+    await res.status(200).json(find_token);
   })
   return router;
 }
