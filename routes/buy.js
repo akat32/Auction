39565cd58729d0238632cc5d71cf9cb0f3237be5
akat32 <baduk32127@gmail.com>
2017-11-17 15:52:56 +0900

    finish buy.js

--- routes/buy.js
+++ routes/buy.js
@@ -1,12 +1,24 @@
 module.exports = (router, Users, List,passport)=>{
   router.post('/', async (req,res)=>{
     var par = req.session.passport.user.final_visit;
+    var uname = req.session.passport.user.name;
+    var utoken = req.session.passport.user.token;
+    var uphone = req.session.passport.user.phone_number;
     console.log(par);
     var item = await List.findOne({item_token : par});
     if(!item) return res.status(404).send('Item Not Found');
     var num = (parseInt(item.now_price) + 0)+ (parseInt(req.body.money) + 0);
-    console.log(num);
+    console.log(num + " " + uname + " " + utoken + " " + uphone);
     List.update({item_token : par}, {$set:{"now_price" : num}}, function(err,result){
+      if(err);
+    });
+    List.update({item_token : par}, {$set:{"buyer_name" : uname}}, function(err,result){
+      if(err);
+    });
+    List.update({item_token : par}, {$set:{"buyer_phone" : uphone}}, function(err,result){
+      if(err);
+    });
+    List.update({item_token : par}, {$set:{"buyer_token" : utoken}}, function(err,result){
       if(err) res.status(500).send('fail');
       else res.status(200).send('succes');
     });
